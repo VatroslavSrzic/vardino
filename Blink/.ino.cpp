dd5ef2a3a9c68a9c861cea21cfa957dce0e6e956
VatroslavSrzic <vatroslav.srzic@gmail.com>
2015-10-08 04:15:42 +0200

    Commit after build

---
+++ Blink/.ino.cpp
@@ -0,0 +1,12 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-10-08 04:11:31
+
+#include "Arduino.h"
+#include <Arduino.h>
+void setup() ;
+void loop() ;
+
+#include "Blink.ino"
+
